67339dca197ebd9c291cd22d9468a3d9ceaa7bdf
Lokhnor <vicoporto@gmail.com>
2024-02-11 16:04:50 +1100

    style: linkedin svg with link

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,27 @@ export default function Home() {
         <div className={styles.spacer}></div>
       </div>
       <div className={styles.content}>
-        <p>Passionate learner, professional Engineer.</p>
+        <p>Bacon ipsum dolor amet cow ground round sirloin shankle.</p>
+        <p>Bacon ipsum dolor amet cow ground round sirloin shankle.</p>
         <p>Bacon ipsum dolor amet cow ground round sirloin shankle.</p>
         <p>🐱‍👤</p>
-        <p></p>
       </div>
       <div className={styles.circle}>
-        <div className={styles.ball}></div>
+        <a
+          href="https://www.linkedin.com/in/vinicius-pinheiro-porto/"
+          target="_blank"
+        >
+          <div className={styles.ball}>
+            <div className={styles.linkedin}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path
+                  fill="#0077B5"
+                  d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+                />
+              </svg>
+            </div>
+          </div>
+        </a>
       </div>
     </main>
   );
